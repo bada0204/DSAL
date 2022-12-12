c52ec331e4474d49d697e5c4ee15bea6a1c141f0
Juho Lee <ask7@naver.com>
2022-12-11 21:35:02 -0500

    DAY 37: permutations

---
+++ java/al/leetcode/medium/problem_0047.java
@@ -0,0 +1,36 @@
+package al.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class problem_0047 {
+
+    public List<List<Integer>> solution(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int[] check = new int[nums.length];
+        Arrays.sort(nums);
+        solution(result, new ArrayList<>(), nums, check, 0);
+        return result;
+    }
+
+    public void solution(List<List<Integer>> result, List<Integer> current, int[] nums, int[] check, int index) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1] && check[i - 1] == 1) {
+                continue;
+            }
+            if (check[i] == 0) {
+                check[i] = 1;
+                current.add(nums[i]);
+                solution(result, current, nums, check, i + 1);
+                current.remove(current.size() - 1);
+                check[i] = 0;
+            }
+        }
+    }
+    
+}
